2207c74df0baa508297c4d0282b43dba5414b1e0
Matviidev <matvii.shpyruk.dev@gmail.com>
2026-01-15 19:25:27 +0200

    feat: add login component

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { loggedIn } from '@angular/fire/auth-guard';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserRoles } from '../model/user-roles';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+
+  pictureUrl$: Observable<string>;
+
+  roles$: Observable<UserRoles>;
+
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.isLoggedIn$ = afAuth.authState.pipe(map((user) => !!user));
+
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
+    this.pictureUrl$ = this.afAuth.authState.pipe(
+      map((user) => (user ? user.photoURL : null))
+    );
+
+    this.roles$ = this.afAuth.idTokenResult.pipe(
+      map((token) => <any>token?.claims ?? { admin: false })
+    );
+  }
+
+  logout() {
+    this.afAuth.signOut();
+    this.router.navigateByUrl('/login');
+  }
+}
