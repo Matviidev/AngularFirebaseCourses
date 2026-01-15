3040e20a0a1274b23a2661f84c230d18a51ed0e9
Matviidev <matvii.shpyruk.dev@gmail.com>
2026-01-15 10:55:46 +0200

    feat: add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,16 @@
 
 export const environment = {
   production: false,
+  useEmulators: true,
   firebase: {
-
+    apiKey: "AIzaSyANP_JZva28wdv_6QaH2ETDt7etnoghgp8",
+    authDomain: "fir-course-b595a.firebaseapp.com",
+    projectId: "fir-course-b595a",
+    storageBucket: "fir-course-b595a.firebasestorage.app",
+    messagingSenderId: "601250983688",
+    appId: "1:601250983688:web:cba12b71a85d9ca1edc6bd",
   },
-  api: {
-
-  }
+  api: {},
 };
 
 /*
@@ -19,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import "zone.js/plugins/zone-error"; // Included with Angular CLI.
